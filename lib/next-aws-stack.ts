1975d0e6ba169165f0aeb2d9b1875ba437ae24c5
Ilham Ghifari <ilham@manyvisual.com>
2024-10-20 17:56:10 +0700

    first commit

--- lib/next-aws-stack.ts
+++ lib/next-aws-stack.ts
@@ -1,16 +1,33 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+
+class ImageGallery extends Construct {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id);
+
+    // this code creates a s3 Bucket
+    new s3.Bucket(this, "OriginalSizeImagesBucket", {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    // this code creates a s3 Bucket
+    new s3.Bucket(this, "ThumbnailSizeImagesBucket", {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+  }
+}
 
 export class NextAwsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new ImageGallery(this, "firstImageGallery");
+    new ImageGallery(this, "secondImageGallery");
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'NextAwsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
